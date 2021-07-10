e1b2e1aaac42e6d4e20a72b482afe1856ddda5ef
Gayan-Muthukumara-Github <85709531+Gayan-Muthukumara-Github@users.noreply.github.com>
2021-07-11 00:10:32 +0530

    first time added

---
+++ Sankgame.py
@@ -0,0 +1,197 @@
+from hashlib import new
+import turtle
+import time
+import random
+
+delay = 0.1
+
+# Score
+score = 0
+high_score = 0
+
+#Set up the screen
+ts = turtle.Screen()
+ts.title("Snake Game")
+ts.bgcolor('black')
+ts.setup(width= 600, height= 600)
+ts.tracer(0) # Turns off the screen updates
+
+#Snake's head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+#Snake's food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments= []
+
+#score board
+sc = turtle.Turtle()
+sc.speed(0)
+sc.shape("square")
+sc.color("aqua")
+sc.penup()
+sc.hideturtle()
+sc.goto(0,260)
+sc.write("Score: 0 High score: 0", align = "center", font=("ariel", 28, "normal"))
+
+score = 0
+high_score = 0
+
+#functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+
+#Functions
+def move():
+    if head.direction == "up":
+        y= head.ycor()
+        head.sety(y + 20)
+    
+    if head.direction == "down":
+        y= head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x= head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x= head.xcor()
+        head.setx(x + 20)
+
+#Keyboard bindings
+
+ts.listen()
+ts.onkeypress(go_up, "w")
+ts.onkeypress(go_up, "Up")
+ts.onkeypress(go_down, "s")
+ts.onkeypress(go_down, "Down")
+ts.onkeypress(go_left, "a")
+ts.onkeypress(go_left, "Left")  
+ts.onkeypress(go_right, "d")
+ts.onkeypress(go_right, "Right")
+
+#Main game loop
+while True:
+    ts.update()
+
+    # Check for a collision with the border
+    if head.xcor() > 290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        # Clear the segments list
+        segments.clear()
+
+        # Reset the score
+        score = 0
+
+        #Reset the delay
+        delay = 0.1
+
+        #Update the score display
+        sc.clear()
+        sc.write("Score: {} High score: {}".format(score, high_score), align="center",font=("ariel", 28, "normal"))
+    
+        
+
+    #Check for a collision with the food
+    if head.distance(food) < 20:
+        # Move the food to a random spot when snake ate the food
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("green")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        #Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+
+        sc.clear()
+        sc.write("Score: {} High score: {}".format(score, high_score), align="center",font=("ariel", 28, "normal"))
+    
+    # Move the end segments first in revers order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    #Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+        
+
+    move()
+
+    #Check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
+            score = 0
+
+            #Reset the delay
+            delay = 0.1
+
+            #Update the score display
+            sc.clear()
+            sc.write("Score: {} High score: {}".format(score, high_score), align="center",font=("ariel", 28, "normal"))
+
+    time.sleep(delay)
+
+
+ts.mainloop()
